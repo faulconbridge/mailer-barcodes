a03d43c4b59b0a3a5f504b19532260cee1a6b5ce
Christopher Wetherill <chris@tbmh.org>
2018-03-11 22:07:26 -0400

    File upload dialog

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,70 @@
 import React from 'react';
 import { withStyles } from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
+import Dropzone from 'react-dropzone';
 import ButtonAppBar from '../components/titlebar';
 
 const styles = theme => ({
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
-    // padding: theme.spacing.unit * 3,
-    minWidth: 0, // So the Typography noWrap works
+    minWidth: 0,
   },
   toolbar: theme.mixins.toolbar,
 });
+
+class Accept extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      accepted: [],
+      rejected: []
+    }
+  }
+
+  render() {
+    return (
+      <section>
+        <div className="dropzone">
+          <Dropzone
+            accept="text/csv"
+            onDrop={(accepted, rejected) => {
+              this.setState({ accepted, rejected }); }
+            }
+            style={{
+              margin: '20px auto',
+              width: '300px',
+              height: '200px',
+              padding: '15px',
+              borderWidth: '2px',
+              borderColor: 'rgb(102, 102, 102)',
+              borderStyle: 'dashed',
+              borderRadius: '5px'}}>
+            <a className="active">
+              Drag and drop your files here to get started, or click to select
+              files to upload.</a>
+          </Dropzone>
+        </div>
+        <aside>
+          <h2>Accepted files</h2>
+          <ul>
+            {
+              this.state.accepted.map(f => 
+                <li key={f.name}>{f.name} - {f.size} bytes</li>)
+            }
+          </ul>
+          <h2>Rejected files</h2>
+          <ul>
+            {
+              this.state.rejected.map(f => 
+                <li key={f.name}>{f.name} - {f.size} bytes</li>)
+            }
+          </ul>
+        </aside>
+      </section>
+    );
+  }
+}
 
 function Home(props) {
   const { classes } = props;
@@ -21,12 +74,12 @@ function Home(props) {
     <div>
       <ButtonAppBar />
       <main className={classes.content}>
-      <div className={classes.toolbar} />
-      <Typography noWrap>
-        {'This is the Home page!'}
-      </Typography>
-    </main>
+        <div className={classes.toolbar} />
 
+        <div className={classes.uploader}>
+          <Accept />
+        </div>
+      </main>
     </div>
   );
 }
